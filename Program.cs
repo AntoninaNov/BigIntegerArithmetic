2ca242f5b1876055fe707695c3cc67fede4970a7
vladshcherbyna <vshcherbyna@kse.org.ua>
2023-05-22 17:00:16 +0300

    add and substract

--- Program.cs
+++ Program.cs
@@ -1,9 +1,28 @@
-﻿public class BigInteger
+﻿using System;
+using System.Text;
+
+public class BigInteger
 {
     private bool _isNegativeNumber;
-    private int[] _reversedDigits;
+    private int[] _digitArray;
+    
+    public BigInteger(string numberAsString)
+    {
+        // Convert string representation to char array and reverse it
+        char[] charArray = numberAsString.ToCharArray();
+        Array.Reverse(charArray);
 
-    public BigInteger(string numericString)
+        // Initialize _digitArray with the same length as the string
+        _digitArray = new int[charArray.Length];
+
+        // Convert each character to an integer and store it in _digitArray
+        for (int i = 0; i < charArray.Length; i++)
+        {
+            _digitArray[i] = int.Parse(charArray[i].ToString());
+        }
+    }
+
+    /*public BigInteger(string numericString)
     {
         _reversedDigits = new int[numericString.Length];
         
@@ -25,7 +44,7 @@
         return new BigInteger(shiftedArray);
     }
 
-    public override string ToString()
+    /*public override string ToString()
     {
         char[] chars = new char[_reversedDigits.Length];
         for (int index = 0; index < _reversedDigits.Length; index++)
@@ -33,12 +52,98 @@
             chars[_reversedDigits.Length - index - 1] = (char)('0' + _reversedDigits[index]);
         }
         return new string(chars);
+    }*/
+    
+    public static BigInteger operator +(BigInteger number1, BigInteger number2)
+    {
+        // Determine the longer and shorter number
+        BigInteger longerNumber = number1;
+        BigInteger shorterNumber = number2;
+
+        if (number2._digitArray.Length > number1._digitArray.Length)
+        {
+            longerNumber = number2;
+            shorterNumber = number1;
+        }
+
+        int maxLength = Math.Max(number1._digitArray.Length, number2._digitArray.Length);
+        int minLength = Math.Min(number1._digitArray.Length, number2._digitArray.Length);
+
+        int[] result = new int[maxLength + 1]; // Additional space for carry
+
+        int carry = 0;
+        int sum;
+
+        for (int i = 0; i < maxLength; i++)
+        {
+            int digit1 = i < longerNumber._digitArray.Length ? longerNumber._digitArray[i] : 0;
+            int digit2 = i < shorterNumber._digitArray.Length ? shorterNumber._digitArray[i] : 0;
+
+            sum = carry + digit1 + digit2;
+            result[i] = sum % 10;
+            carry = sum / 10;
+        }
+
+        if (carry > 0)
+        {
+            result[maxLength] = carry;
+        }
+
+        BigInteger sumNumber = new BigInteger("");
+        sumNumber._digitArray = result;
+
+        return sumNumber;
+    }
+    
+    public static BigInteger operator -(BigInteger number1, BigInteger number2)
+    {
+        int maxLength = Math.Max(number1._digitArray.Length, number2._digitArray.Length);
+        int[] result = new int[maxLength];
+        int borrow = 0;
+
+        for (int i = 0; i < maxLength; i++)
+        {
+            int digit1 = i < number1._digitArray.Length ? number1._digitArray[i] : 0;
+            int digit2 = i < number2._digitArray.Length ? number2._digitArray[i] : 0;
+
+            int difference = digit1 - borrow - digit2;
+
+            if (difference < 0)
+            {
+                difference += 10;
+                borrow = 1;
+            }
+            else
+            {
+                borrow = 0;
+            }
+
+            result[i] = difference;
+        }
+
+        BigInteger differenceNumber = new BigInteger("");
+        differenceNumber._digitArray = result;
+
+        return differenceNumber;
+    }
+    
+    public override string ToString()
+    {
+        // Convert _digitArray to string in forward order and return it
+        StringBuilder sb = new StringBuilder(_digitArray.Length);
+
+        for (int i = 0; i < _digitArray.Length; i++)
+        {
+            sb.Append(_digitArray[i]);
+        }
+
+        return sb.ToString();
     }
 
     private int[] GetDigitsFromHalf(int startIndex, int endIndex)
     {
         int[] halfDigits = new int[endIndex - startIndex + 1];
-        Array.Copy(_reversedDigits, startIndex, halfDigits, 0, endIndex - startIndex + 1);
+        Array.Copy(_digitArray, startIndex, halfDigits, 0, endIndex - startIndex + 1);
         return halfDigits;
     }
 
@@ -82,17 +187,44 @@
     }
     */
     
-    public static BigInteger operator +(BigInteger a, BigInteger b) => a.Add(b);
-    public static BigInteger operator -(BigInteger a, BigInteger b) => a.Sub(b);
-    public static BigInteger operator *(BigInteger a, BigInteger b) => a.Multiply(b);
+    //public static BigInteger operator +(BigInteger a, BigInteger b) => a.Add(b);
+    //public static BigInteger operator -(BigInteger a, BigInteger b) => a.Sub(b);
+    //public static BigInteger operator *(BigInteger a, BigInteger b) => a.Multiply(b);
 }
 
 class Program
 {
     static void Main(string[] args)
     {
-        var x = new BigInteger("1313234242425");
-        Console.WriteLine(x);
+        //var x = new BigInteger("1313234242425");
+        //Console.WriteLine(x);
+        
+        // Checking for the string "123434"
+        BigInteger number1 = new BigInteger("123434");
+        Console.WriteLine(number1.ToString());
+
+        // Checking for the string "9876543210"
+        BigInteger number2 = new BigInteger("9876543210");
+        Console.WriteLine(number2.ToString());
+
+        // Checking for the string "0"
+        BigInteger number3 = new BigInteger("0");
+        Console.WriteLine(number3.ToString());
+
+        // Checking for an empty string
+        BigInteger number4 = new BigInteger("");
+        Console.WriteLine(number4.ToString());
+        
+        // Addition check
+        BigInteger number5 = new BigInteger("12");
+        BigInteger number6 = new BigInteger("67");
+
+        BigInteger sum = number5 + number6;
+        BigInteger difference = number6 - number5;
+
+        Console.WriteLine("Sum: " + sum);
+        Console.WriteLine("Difference: " + difference);
+    }
         
         /*Console.WriteLine("Enter the first number:");
         string firstNumber = Console.ReadLine();
@@ -116,5 +248,5 @@ class Program
         Console.WriteLine("The result of the multiplication is: ");
         Console.WriteLine(resultMultiply);
         */
-    }
+    //}
 }
